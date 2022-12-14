e2731d869ae0e2f8898fe14588ae9e53fd0a09c3
jyaru1110 <odoo@ampd02.odoo.com>
2022-12-14 06:27:07 +0000

    Primer commit

---
+++ app_delta/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'delta',
+    'summary': """Custom app for delta project""",
+    'description':"""
+           App to customize delta requirements
+    """,
+    'author' : 'flow',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['sale','stock'],
+    'data':[
+    ],
+    
+    'demo' : [
+        
+    ],
+    'license': 'OPL-1'
+}
