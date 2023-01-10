42411a2e9ad1459f0a4a0b831c8f238248613d45
jyaru1110 <odoo@ampd02.odoo.com>
2023-01-10 15:39:58 +0000

    sale order create override

--- app_delta/models/.ipynb_checkpoints/sale_order-checkpoint.py
+++ app_delta/models/.ipynb_checkpoints/sale_order-checkpoint.py
@@ -4,3 +4,11 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     empaque = fields.Selection(string="Empaque",selection=[('P','Plegado'),('D','Desplegado')])
+    folio_vendedor = fields.Text(string="Folio por vendedor")
+    
+    @api.model_create_multi
+    def create(self,values):
+        num_folios = len(self.env['sale.order'].search_read([('user_id','=',values['user_id'])],['id']))
+        values['folio_vendedor'] = self.env['res.users'].search_read([('user_id','=',values['user_id'])],['prefijo']) + str(num_folios)
+        override_create = super(SaleOrder,self).create(values)
+        return override_create
